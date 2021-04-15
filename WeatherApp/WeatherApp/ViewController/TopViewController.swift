8a9c5f85ca31742cb5feb6fd2d35733f7d47e2eb
shusuke0812 <shu.ota0812@gmail.com>
2021-04-15 22:11:16 +0900

    [add]ViewControllerにViewModelを接続し、Reloadボタンを押したら天気イメージを更新する処理を追加

--- WeatherApp/WeatherApp/ViewController/TopViewController.swift
+++ WeatherApp/WeatherApp/ViewController/TopViewController.swift
@@ -10,18 +10,36 @@ import UIKit
 class TopViewController: UIViewController {
     /// BaseView
     private var baseView: TopBaseView { self.view as! TopBaseView }
+    /// ViewModel
+    private var viewModel: TopViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = TopViewModel(weatherRepository: WeatherRepository())
+        self.setDelegate()
     }
 }
+// MARK: - Initialization
 extension TopViewController {
     private func setDelegate() {
         self.baseView.delegate = self
+        self.viewModel.delegate = self
     }
 }
 // MARK: - BaseView Delegate Method
 extension TopViewController: TopBaseViewDelegate {
     func didTapReloadButton() {
+        self.viewModel.getWeatherInfo()
+    }
+}
+// MARK: - ViewModel Delegate Method
+extension TopViewController: TopViewModelDelegate {
+    func didSuccessWeatherInfo() {
+        DispatchQueue.main.async {
+            self.baseView.setWeatherImageView(weather: self.viewModel.weather)
+        }
+    }
+    func didFailedWeatherInfo(errorMessage: String) {
+        print(errorMessage)
     }
 }
